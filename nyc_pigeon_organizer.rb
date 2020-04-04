b865002f0153588cd2911272cf4844f32be01b7a
Cameron Rhinehardt <rhinehardtc@gmail.com>
2020-04-04 23:21:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,8 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   by_name = {}
-  data.each(:color) do |color|
-    by_name = color
+  data.each(:color) do |color, name|
+    by_name = name
     binding.pry
   end
   by_name
